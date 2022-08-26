202a9bb6ca1f21d6a063ae709611fa33dd127a37
njh <wzunjh@qq.com>
2022-08-26 23:15:03 +0800

    测试test包

---
+++ reggie_take_out/src/test/java/com/njh/ReggieTakeOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.njh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReggieTakeOutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
