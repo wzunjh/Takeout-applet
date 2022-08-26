7e0cb23219c7227957797364d745b53c1e32e1da
njh <wzunjh@qq.com>
2022-08-27 01:38:15 +0800

    测试test包

--- reggie_take_out/src/test/java/com/njh/ReggieTakeOutApplicationTests.java
+++ reggie_take_out/src/test/java/com/njh/ReggieTakeOutApplicationTests.java
@@ -1,13 +1,26 @@
 package com.njh;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 @SpringBootTest
+
 class ReggieTakeOutApplicationTests {
 
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Test
-    void contextLoads() {
+    void textString() {
+
+        //ÆÕÍ¨¼üÖµ¶Ô
+        redisTemplate.opsForValue().set("n","123");
+
+
     }
 
 }
