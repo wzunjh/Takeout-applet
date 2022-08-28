594ecab1743c247a34ce2efd76d3ba8a6c2ee42c
njh <wzunjh@qq.com>
2022-08-28 18:59:45 +0800

    乐观锁

--- reggie_take_out/src/main/java/com/njh/common/BaseContext.java
+++ reggie_take_out/src/main/java/com/njh/common/BaseContext.java
@@ -1,6 +1,9 @@
 package com.njh.common;
 
 public class BaseContext {
+
+    //多线程访问同一个共享变量为了防止高并发，采用加锁同步方式
+
     private static ThreadLocal<Long> threadLocal =new ThreadLocal<>();
 
     public static void setCurrentId(Long id){
