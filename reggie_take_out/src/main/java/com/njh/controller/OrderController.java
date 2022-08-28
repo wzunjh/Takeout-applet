78ad8ed27d0225f2ffd8f468fdc37676443f3797
njh <wzunjh@qq.com>
2022-08-28 18:06:17 +0800

    再来一单功能完善

--- reggie_take_out/src/main/java/com/njh/controller/OrderController.java
+++ reggie_take_out/src/main/java/com/njh/controller/OrderController.java
@@ -2,17 +2,25 @@ package com.njh.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.njh.common.BaseContext;
 import com.njh.common.R;
 import com.njh.domain.OrderDetail;
 import com.njh.domain.Orders;
+import com.njh.domain.ShoppingCart;
 import com.njh.dto.OrderStatus;
 import com.njh.dto.OrdersDto;
 import com.njh.service.OrderDetailService;
 import com.njh.service.OrderService;
+import com.njh.service.ShoppingCartService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -22,6 +30,9 @@ public class OrderController {
     private OrderService orderService;
     @Autowired
     private OrderDetailService orderDetailService;
+
+    @Autowired
+    private ShoppingCartService shoppingCartService;
 
     @PostMapping("/submit")
     public R<String> submit(@RequestBody Orders orders) {
@@ -60,10 +71,47 @@ public class OrderController {
 
 
     @PostMapping("/again")
-    public R<String> addOrderAgain(@RequestBody Orders orders) {
-        if (orders.getId() != null) {
-            return R.success("成功！");
-        }
-        return R.error("失败!");
+    public R<String> againSubmit(@RequestBody Map<String,String> map){
+        String ids = map.get("id");
+
+        long id = Long.parseLong(ids);
+
+        LambdaQueryWrapper<OrderDetail> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(OrderDetail::getOrderId,id);
+        //获取该订单对应的所有的订单明细表
+        List<OrderDetail> orderDetailList = orderDetailService.list(queryWrapper);
+
+        //通过用户id把原来的购物车给清空，这里的clean方法是视频中讲过的,建议抽取到service中,那么这里就可以直接调用了
+        Long userId = BaseContext.getCurrentId();
+        LambdaQueryWrapper<ShoppingCart> queryWrapper1 = new LambdaQueryWrapper<>();
+        queryWrapper1.eq(ShoppingCart::getUserId,userId);
+        shoppingCartService.remove(queryWrapper1);
+
+        List<ShoppingCart> shoppingCartList = orderDetailList.stream().map((item) -> {
+            //把从order表中和order_details表中获取到的数据赋值给这个购物车对象
+            ShoppingCart shoppingCart = new ShoppingCart();
+            shoppingCart.setUserId(userId);
+            shoppingCart.setImage(item.getImage());
+            Long dishId = item.getDishId();
+            Long setmealId = item.getSetmealId();
+            if (dishId != null) {
+                //如果是菜品那就添加菜品的查询条件
+                shoppingCart.setDishId(dishId);
+            } else {
+                //添加到购物车的是套餐
+                shoppingCart.setSetmealId(setmealId);
+            }
+            shoppingCart.setName(item.getName());
+            shoppingCart.setDishFlavor(item.getDishFlavor());
+            shoppingCart.setNumber(item.getNumber());
+            shoppingCart.setAmount(item.getAmount());
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            return shoppingCart;
+        }).collect(Collectors.toList());
+
+        //把携带数据的购物车批量插入购物车表  这个批量保存的方法要使用熟练！！！
+        shoppingCartService.saveBatch(shoppingCartList);
+
+        return R.success("操作成功");
     }
 }
